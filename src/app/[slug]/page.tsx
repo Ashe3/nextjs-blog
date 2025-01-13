f9cecd2fecb9039a39dd840c0bc2d1e0df92e242
Anton Shevchenko <korolewsky.anton3@gmail.com>
2025-01-13 02:07:56 +0200

    fixed some padding

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -16,7 +16,7 @@ const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
 
   return (
     <main>
-      <article className="p-52 pt-16">
+      <article className="p-3 md:p-20 lg:px-32">
         <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
         <p className="text-gray-500">{post.date}</p>
         <Markdown className="mt-4">{post.content}</Markdown>
